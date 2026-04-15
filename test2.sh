a7db5abc60de084df44a4af744f6997a47d1fc28
saihgupr <121105543+saihgupr@users.noreply.github.com>
2026-04-15 10:53:40 -0400

    Cleanup redundant files and update README images

--- test2.sh
+++
@@ -1,3 +0,0 @@
-curl -v -X POST http://192.168.1.2:8080/api/config \
-     -H "Content-Type: application/json" \
-     -d '{"masterEnabled": true, "nfcEnabled": true, "webUIEnabled": true, "triggers": {}, "favoriteTriggers": []}'
